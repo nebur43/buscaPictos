324806d4ff7ae3621edde4493f1b0083734151ca
ruben.aguado <ruben.aguado>
2026-04-02 21:01:54 +0200

    manejo de horizontal y vertical

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,31 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import React, { useEffect } from 'react';
+import * as Device from 'expo-device';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    async function configureOrientation() {
+      try {
+        const deviceType = await Device.getDeviceTypeAsync();
+        
+        // DeviceType: 1 is PHONE, 2 is TABLET, 3 is DESKTOP
+        if (deviceType === Device.DeviceType.PHONE) {
+          await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+        } else {
+          await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+        }
+      } catch (error) {
+        console.log("Could not configure orientation lock", error);
+      }
+    }
+    configureOrientation();
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
